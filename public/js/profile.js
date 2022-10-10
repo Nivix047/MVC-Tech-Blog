f5a9085c334ea68565352132d67aba23f74bfc47
Daniel Xu <xudh47@gmail.com>
2022-10-09 18:04:35 -0700

    Updated handlebars and databases

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,17 +2,14 @@ const projectFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector("#project-name").value.trim();
-  const needed_funding = document
-    .querySelector("#project-funding")
-    .value.trim();
   const description = document
     .querySelector("#project-description")
     .value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch("/api/projects", {
       method: "POST",
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, description }),
       headers: { "Content-Type": "application/json" },
     });
 
